04f4eed570096590c36003a875bca6c32c089651
Dylan Cairns-Howarth <dpch@hotmail.co.uk>
2021-09-07 04:59:33 +0100

    Clean up keys[]
    For better readability

--- vswm.c
+++ vswm.c
@@ -38,19 +38,20 @@ static Window root;
 static int screen, width, height;
 
 static Key keys[] = {
-	{ Mod4Mask, XK_Return, launch, "xterm" },
-	{ Mod4Mask, XK_d, launch, "dmn" },
-	{ Mod4Mask, XK_b, launch, "chromium" },
-	{ Mod4Mask, XK_p, launch, "scr" },
-	{ Mod4Mask | ShiftMask, XK_q, destroy, 0 },
-	{ Mod4Mask | ShiftMask, XK_r, refresh, 0 },
-	{ Mod4Mask, XK_Tab, focus, "next" },
-	{ Mod4Mask | ShiftMask, XK_Tab, focus, "prev" },
-	{ 0, XF86XK_AudioMute, launch, "pamixer -t" },
-	{ 0, XF86XK_AudioLowerVolume, launch, "pamixer -d 5" },
-	{ 0, XF86XK_AudioRaiseVolume, launch, "pamixer -i 5" },
-	{ 0, XF86XK_MonBrightnessDown, launch, "xbacklight -dec 5" },
-	{ 0, XF86XK_MonBrightnessUp, launch, "xbacklight -inc 5" },
+	{ Mod4Mask,             XK_Return,                  launch,     "xterm"             },
+	{ Mod4Mask,             XK_d,                       launch,     "dmn"               },
+	{ Mod4Mask,             XK_b,                       launch,     "chromium"          },
+	{ Mod4Mask,             XK_p,                       launch,     "scr"               },
+	{ Mod4Mask,             XK_q,                       destroy,    0                   },
+	{ Mod4Mask | ShiftMask, XK_r,                       refresh,    0                   },
+	{ Mod4Mask | ShiftMask, XK_q,                       destroy,    0                   },
+	{ Mod4Mask,             XK_Tab,                     focus,      "next"              },
+	{ Mod4Mask | ShiftMask, XK_Tab,                     focus,      "prev"              },
+	{ 0,                    XF86XK_AudioMute,           launch,     "pamixer -t"        },
+	{ 0,                    XF86XK_AudioLowerVolume,    launch,     "pamixer -d 5"      },
+	{ 0,                    XF86XK_AudioRaiseVolume,    launch,     "pamixer -i 5"      },
+	{ 0,                    XF86XK_MonBrightnessDown,   launch,     "xbacklight -dec 5" },
+	{ 0,                    XF86XK_MonBrightnessUp,     launch,     "xbacklight -inc 5" },
 };
 
 static const Events events[LASTEvent] = {
